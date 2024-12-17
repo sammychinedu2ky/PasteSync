414e4afa995bd42cdfb3bbf54edc5f758e0d0e08
Samson Amaugo <sammychinedu2ky@gmail.com>
2024-12-17 10:00:34 +0100

    added custom resource commands

--- AspireHost/Program.cs
+++ AspireHost/Program.cs
@@ -1,12 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache").WithRedisInsight().WithAddCustomPage();
+
 var backendApi = builder.AddProject<Projects.backend>("backend");
-var frontend = builder.AddNpmApp("frontend", "../frontend", "dev");
 backendApi.WithReference(cache)
 .WithHttpEndpoint(name: "backend", port: 5000)
 .WaitFor(cache);
 
+var frontend = builder.AddNpmApp("frontend", "../frontend", "dev");
 frontend.WithReference(backendApi)
  .WithHttpEndpoint(env: "PORT", port: 3000)
+ .WithExternalHttpEndpoints()
 .WaitFor(backendApi);
 builder.Build().Run();
